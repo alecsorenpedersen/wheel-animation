d82a87464cb2ca91afafe9c4cc2381b97e12c5d8
Alec Pedersen <96690104+alecsorenpedersen@users.noreply.github.com>
2023-05-18 09:36:20 +0100

    babel config

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-	presets: ['@babel/preset-env', '@babel/preset-react'],
-	plugins: [
-		'@babel/plugin-proposal-class-properties',
-		'@babel/plugin-transform-runtime',
+	presets: [
+		'@babel/preset-env',
+		'@babel/preset-react',
+		'@babel/preset-typescript',
 	],
 };
